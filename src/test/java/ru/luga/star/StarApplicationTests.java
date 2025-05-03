7ab628637beac38a7152d3b35bc893efe8f512b6
Vasiliy Rubtsov <vasiliy.rubtsov@rtmis.ru>
2025-05-03 20:14:33 +0300

    Initial Commit

---
+++ src/test/java/ru/luga/star/StarApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.luga.star;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
